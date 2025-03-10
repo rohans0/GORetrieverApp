d8ddcf722150ecda384a1e3a64e8cd05061635fa
rohans0 <159118768+rohans0@users.noreply.github.com>
2025-03-09 21:33:55 -0700

    basic gui

--- predict.py
+++ predict.py
@@ -1,4 +1,4 @@
-import os
+import sys, os
 import argparse
 parser = argparse.ArgumentParser(description='manual to this script')
 parser.add_argument("--gpu", type=str, default='0')
@@ -10,21 +10,64 @@ args = parser.parse_args()
 os.environ["CUDA_VISIBLE_DEVICES"] = args.gpu
 print(args)
 
-import json
 from pyserini import search
 from pyserini.search import SimpleSearcher
 # from pyserini.search.lucene import LuceneSearcher
 from pygaggle.rerank.base import Query, Text
 from pygaggle.rerank.transformer import MonoT5
-from tqdm import tqdm
-import numpy as np
-import requests
-from bs4 import BeautifulSoup as bs
-import random 
 import nltk
 from sentence_transformers import CrossEncoder
 from transformers import T5ForConditionalGeneration
 import torch.nn as nn
+
+import json
+import numpy as np
+
+from tqdm import tqdm
+from bs4 import BeautifulSoup as bs
+
+# gui
+from PyQt6.QtCore import Qt
+from PyQt6.QtWidgets import (
+    QAbstractItemView,
+    QApplication,
+    QBoxLayout,
+    QLabel,
+    QPushButton,
+    QMainWindow,
+    QTableWidget,
+    QTableWidgetItem,
+    QVBoxLayout,
+    QWidget,
+    QFileDialog,
+    QScrollArea,
+    QLayoutItem 
+)
+
+# Main window
+app = QApplication(sys.argv)
+window = QMainWindow()
+window.setWindowTitle("goretreiver")
+window.resize(800, 600)
+window.show()
+
+# Scroll area
+scroll_area = QScrollArea()
+scroll_area.setWidgetResizable(True)
+scroll_area.setHorizontalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAsNeeded)
+scroll_area.setVerticalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOn)
+window.setCentralWidget(scroll_area)
+
+# Container widget
+container_widget = QWidget()
+scroll_area.setWidget(container_widget)
+
+# Create layout
+layout = QVBoxLayout(container_widget)
+layout.setContentsMargins(0, 0, 0, 0)
+layout.setSpacing(0)
+
+
 
 task_def = {
     'cc': 'Cellular Component',
@@ -32,68 +75,19 @@ task_def = {
     'mf': 'Molecular Function',
 }
 
-def get_text(url): 
-    headers = {} 
-    user_agent_list = [
-        "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36",
-        "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36",
-        "Mozilla/5.0 (Windows NT 10.0; WOW64) Gecko/20100101 Firefox/61.0",
-        "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.186 Safari/537.36",
-        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.62 Safari/537.36",
-        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.101 Safari/537.36",
-        "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)",
-        "Mozilla/5.0 (Macintosh; U; PPC Mac OS X 10.5; en-US; rv:1.9.2.15) Gecko/20110303 Firefox/3.6.15",
-    ]
-    headers['User-Agent'] = random.choice(user_agent_list)
-
-    content = requests.get(url, headers = headers).text
-    # time.sleep(10)
-    soup = bs(content, 'lxml')
-    # text = ''
-    try:
-        title = soup.select('#full-view-heading > h1')[0].get_text().replace('\n', '').replace('\t', '').strip()
-    except IndexError:
-        # print(url)
-        text = ''
-    else:
-        try:
-            abstracts = soup.select('#eng-abstract > p')
-            abstract = ''
-            for item in abstracts:
-                abstract = abstract + item.get_text().replace('\n', '').replace('\t', '').strip()
-            # abstract = soup.select('#eng-abstract > p')[0].get_text().replace('\n', '').strip()
-            text = title+'.'+abstract
-        except IndexError:
-            text=''
-     
-    return text
+proid2name = np.load('./file/proid2name.npy', allow_pickle=True).item()
+pmid2text = np.load('./file/pmid2text.npy', allow_pickle=True).item()
 
 
-def data_extract(args):
-    '''
-    sentence extract
-    return:
-        pro2text[dictionary]:{
-            'protein_id': extracted context(string)
-        }
-    '''
-    save_file = f'./test/{args.task}_dev_t5_texts.npy'
-    print(save_file)
-    if os.path.exists(save_file):
-        data = np.load(save_file, allow_pickle=True).item()
-        print('load from: ', save_file)
-        print('data extract end!')
-        return data
+def draw_table(table):
+    pass
+
+def get_input_data(file):
+    
     tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
-    model = T5ForConditionalGeneration.from_pretrained('castorini/monot5-base-med-msmarco')
-    model = nn.DataParallel(model)
-    model = model.cuda().module
-    T5tokenizer = MonoT5.get_tokenizer('t5-base')
-    reranker = MonoT5(model, T5tokenizer)
-    proid2name = np.load('./file/proid2name.npy', allow_pickle=True).item()
-    pmid2text = np.load('./file/pmid2text.npy', allow_pickle=True).item()
+    
     pro2text = {}
-    with open(args.file) as f:
+    with open(file) as f:
         lines = f.readlines()
         for line in tqdm(lines):
             line = line.split('\t')
@@ -115,116 +109,277 @@ def data_extract(args):
                 continue
             if sentences[0] in pro2text[pro]:
                 continue
+
             pro2text[pro].extend(sentences)
 
-    text_score = {}
-    for pro in tqdm(pro2text): 
-        query = f"what is the {task_def[args.task]} of protein {proid2name[pro]}?"
-        sentences = pro2text[pro]
-        if len(sentences) < 3:
-            print(sentences)
-            continue
-        texts = []
-        for sentence in sentences:
-            texts.append(Text(sentence, {}, 0))
-        scores = reranker.rerank(Query(query), texts)
-        reranked = {}
-        for i in range(len(scores)):
-            reranked[sentences[i]] = float(scores[i].score)
-        text_score[pro] = reranked
 
-        reranked = sorted(reranked, key=lambda x:reranked[x], reverse=True)
-        res = reranked[:len(reranked)//2]
-        pro2text[pro] = ' '.join(res)
-        
 
-    np.save(save_file.replace('texts', 'texts_scores'), text_score)
-    np.save(save_file, pro2text)
-    print('data extract end!')
+
+    # Table
+    table = QTableWidget(len(pro2text), 2)
+    table.setHorizontalHeaderLabels(['Protein ID', 'Tokenized Sentences'])
+
+    table.setWordWrap(True)
+    table.setEditTriggers(table.EditTrigger.NoEditTriggers)
+    table.setVerticalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOn)
+    table.setMinimumHeight(600)
+
+    row = 0
+    for key, value in pro2text.items():
+        table.setItem(row, 0, QTableWidgetItem(key))
+        # table.setItem(row, 1, QTableWidgetItem(key))
+        table.setItem(row, 1, QTableWidgetItem('\n'.join(value)))
+
+        table.setRowHeight(row, 10*len(value))
+
+        row += 1
+
+    table.resizeColumnsToContents()
+
+    
+    global layout
+    layout.addWidget(QLabel("Input data:"))
+    layout.addWidget(table)
+
+
+    QApplication.processEvents()
+    window.update()
+
     return pro2text
 
 
+def data_extract(args):
+    '''
+    sentence extract
+    return:
+        pro2text[dictionary]:{
+            'protein_id': extracted context(string)
+        }
+    '''
+    pro2text = get_input_data(args.file)
+    
+    save_file = f'./test/{args.task}_dev_t5_texts.npy'
+    print(save_file)
+    if os.path.exists(save_file):
+        data = np.load(save_file, allow_pickle=True).item()
+        print('load from: ', save_file)
+        print('data extract end!')
+    else:
+        # load models
+        model = T5ForConditionalGeneration.from_pretrained('castorini/monot5-base-med-msmarco')
+        model = nn.DataParallel(model)
+        model = model.cpu().module
+        T5tokenizer = MonoT5.get_tokenizer('t5-base')
+        reranker = MonoT5(model, T5tokenizer)
+
+        text_score = {}
+        for pro in tqdm(pro2text): 
+            query = f"what is the {task_def[args.task]} of protein {proid2name[pro]}?"
+            sentences = pro2text[pro]
+            if len(sentences) < 3:
+                print(sentences)
+                continue
+            texts = []
+            for sentence in sentences:
+                texts.append(Text(sentence, {}, 0))
+            scores = reranker.rerank(Query(query), texts)
+            reranked = {}
+            for i in range(len(scores)):
+                reranked[sentences[i]] = float(scores[i].score)
+            text_score[pro] = reranked
+
+            reranked = sorted(reranked, key=lambda x:reranked[x], reverse=True)
+            res = reranked[:len(reranked)//2]
+            pro2text[pro] = ' '.join(res)
+
+        np.save(save_file.replace('texts', 'texts_scores'), text_score)
+        np.save(save_file, pro2text)
+        print('data extract end!')
+
+        data = pro2text
+
+
+
+    # Table
+    # table = QTableWidget(len(data), 2)
+    # table.setWordWrap(True)
+    # table.setEditTriggers(table.EditTrigger.NoEditTriggers)
+    # table.setVerticalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOn)
+    # table.setHorizontalHeaderLabels(['Protein ID', 'Tokenized Sentences'])
+    # table.setMinimumHeight(400)
+    #
+    # row = 0
+    # for key, value in data.items():
+    #     table.setItem(row, 0, QTableWidgetItem(key))
+    #     # table.setItem(row, 1, QTableWidgetItem(key))
+    #     table.setItem(row, 1, QTableWidgetItem(', '.join(value)))
+    #
+    #     table.setRowHeight(row, 10*len(value))
+    #
+    #     row += 1
+    #
+    # table.resizeColumnsToContents()
+    #
+    # layout.addWidget(QLabel("Input data:"))
+    # layout.addWidget(table)
+    #
+    # QApplication.processEvents()
+    # window.update()
+
+    return data
+
+
 def all_retrieval_dict(args):
+
+    # make or get numpy cached file of current step
     save_file = f'./test/{args.task}_retrieval_all.npy'
     if os.path.exists(save_file):
         data = np.load(save_file, allow_pickle=True).item()
         print(save_file)
         print("All retrieval end!") 
-        return data
-    searcher = SimpleSearcher('./pro_index/')
-    pro2text = data_extract(args)
-    pro2go = np.load(f'./file/{args.task}_pro2go.npy', allow_pickle=True).item()
-    retrieval_dict = {}
-    for proid in tqdm(pro2text):
-        k = 0
-        res = []
-        try:
-            proname = json.loads(searcher.doc(proid).raw())['contents']
-        except AttributeError:
-            print('Missing Protein Name', proname)
-            continue
-        
-        l = searcher.search(proname, 3000)
-        for _ in l:
-            if k > int(args.pro) + 2:
-                break
-            _ = json.loads(_.raw)
-            if _['id'] == proid:
+
+    else:
+        searcher = SimpleSearcher('./pro_index/')
+        pro2text = data_extract(args)
+        pro2go = np.load(f'./file/{args.task}_pro2go.npy', allow_pickle=True).item()
+        retrieval_dict = {}
+        for proid in tqdm(pro2text):
+            k = 0
+            res = []
+            try:
+                proname = json.loads(searcher.doc(proid).raw())['contents']
+            except AttributeError:
+                print('Missing Protein Name', proname)
                 continue
-            else:
-                if pro2go.get(_['id']):
-                    k += 1
-                    res.append(pro2go[_['id']])
+            
+            l = searcher.search(proname, 3000)
+            for _ in l:
+                if k > int(args.pro) + 2:
+                    break
+                _ = json.loads(_.raw)
+                if _['id'] == proid:
+                    continue
                 else:
-                    res.append([])
-        if k < int(args.pro) + 2:
-            print(k, proid)
-        retrieval_dict[proid] = res
-    np.save(save_file, retrieval_dict)
-    print('write:', save_file)
-    print("All retrieval end!") 
-    return retrieval_dict      
+                    if pro2go.get(_['id']):
+                        k += 1
+                        res.append(pro2go[_['id']])
+                    else:
+                        res.append([])
+            if k < int(args.pro) + 2:
+                print(k, proid)
+            retrieval_dict[proid] = res
+        np.save(save_file, retrieval_dict)
+        print('write:', save_file)
+        print("All retrieval end!") 
+        data = retrieval_dict      
+
+
+    # Table
+    # table = QTableWidget(len(data), 2)
+    # table.setWordWrap(True)
+    # table.setEditTriggers(table.EditTrigger.NoEditTriggers)
+    # table.setHorizontalHeaderLabels(['Protein ID', 'Tokenized Sentences'])
+    # table.setMinimumHeight(1000)
+    #
+    # row = 0
+    # for key, value in data.items():
+    #     table.setItem(row, 0, QTableWidgetItem(key))
+    #     # table.setItem(row, 1, QTableWidgetItem(key))
+    #     table.setItem(row, 1, QTableWidgetItem('\n '.join(value)))
+    #
+    #     # table.setRowHeight(row, 10*len(value))
+    #
+    #     row += 1
+    #
+    # table.resizeColumnsToContents()
+    # table.resizeRowsToContents()
+    #
+    # layout.addWidget(QLabel("All retrieval dict:"))
+    # layout.addWidget(table)
+    #
+    # QApplication.processEvents()
+    # window.update()
+
+
+    return data
 
 def retrieval(args):
+    # make or get numpy cached file of current step
     save_file = f'./test/{args.task}_retrieval.npy'
     if args.pro != '0':
         save_file = save_file.replace('retrieval', f'retrieval_pro_{args.pro}')
+
     if os.path.exists(save_file):
         data = np.load(save_file, allow_pickle=True).item()
         print(save_file)
         print("retrieval end!") 
-        return data
-    retrieval_dict = all_retrieval_dict(args)
-    d = {}
-    for proid in tqdm(retrieval_dict):
-        k = 0
-        res = []
-        for l in retrieval_dict[proid]:
-            if k==int(args.pro):
-                # print(k)
-                break
-            if len(l) == 0:
-                continue
-            else:
-                k += 1
-                res.extend(l)
-        d[proid] = res
 
-    np.save(save_file, d)
-    print("write:", save_file)
-    print("retrieval end!")
-    return d
+    else:
+        retrieval_dict = all_retrieval_dict(args)
+        d = {}
+        for proid in tqdm(retrieval_dict):
+            k = 0
+            res = []
+            for l in retrieval_dict[proid]:
+                if k==int(args.pro):
+                    # print(k)
+                    break
+                if len(l) == 0:
+                    continue
+                else:
+                    k += 1
+                    res.extend(l)
+            d[proid] = res
+
+        np.save(save_file, d)
+        print("write:", save_file)
+        print("retrieval end!")
+
+        data = d
+
+    table = QTableWidget(len(data), 2)
+    table.setHorizontalHeaderLabels(['Protein ID', 'GO IDs'])
+
+    table.setWordWrap(True)
+    table.setEditTriggers(table.EditTrigger.NoEditTriggers)
+    table.setVerticalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOn)
+    table.setMinimumHeight(500)
+
+
+    row = 0
+    for key, value in data.items():
+        table.setItem(row, 0, QTableWidgetItem(key))
+        # table.setItem(row, 1, QTableWidgetItem(key))
+        table.setItem(row, 1, QTableWidgetItem('\n '.join(value)))
+
+        table.setRowHeight(row, 10*len(value))
+
+        row += 1
+
+    table.resizeColumnsToContents()
+
+    layout.addWidget(QLabel("Retrieval dict:"))
+    layout.addWidget(table)
+
+    QApplication.processEvents()
+    window.update()
+
+
+    return data
 
 def rerank(args):
-    retrieval_data = retrieval(args)
     pro2text = data_extract(args)
+    retrieval_data = retrieval(args)
     score = {}
+
+    # make or get numpy cached file of current step
     score_dict = f'./test/{args.task}_t5_scores.npy'
     if os.path.exists(score_dict):
         print("score cache: ", score_dict)
         score = np.load(score_dict, allow_pickle=True).item()
+    # if we have a cache , 
+
     goreader = SimpleSearcher('./go_index/')
-    proid2name = np.load('./file/proid2name.npy', allow_pickle=True).item()
     model = CrossEncoder(f'./cross_model/{args.task}_PubMedBERT_epoch1/', max_length=512)
     data = []
     
@@ -264,6 +419,18 @@ def rerank(args):
             
     np.save(score_dict, score)  
 
+
+
+
+    table = QTableWidget(2000, 3)
+    table.setHorizontalHeaderLabels(['Protein ID', 'GO ID', 'Score'])
+
+    table.setWordWrap(True)
+    table.setEditTriggers(table.EditTrigger.NoEditTriggers)
+    table.setVerticalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOn)
+    table.setMinimumHeight(1000)
+
+    row = 0
     for proid in tqdm(retrieval_data):
         if len(retrieval_data[proid]) == 0:
             print("proid", proid)
@@ -275,8 +442,14 @@ def rerank(args):
         for goid in retrieval_data[proid]:
             res[goid] = str(score[proid].get(goid, 0))
         res = sorted(res.items(), key=lambda x: x[1], reverse=True)[:50]
+
         for item in res:
             data.append(proid + '\t' + item[0] + '\t' + item[1] + '\n')
+            table.setItem(row, 0, QTableWidgetItem(proid))
+            table.setItem(row, 1, QTableWidgetItem(item[0]))
+            table.setItem(row, 2, QTableWidgetItem(item[1]))
+
+            row += 1
 
     
     save_file = f'./result/{args.task}_t5_dev_rerank.txt'
@@ -288,5 +461,61 @@ def rerank(args):
     print('rerank end!')
 
 
-if __name__ == '__main__':
+
+    table.resizeColumnsToContents()
+    table.resizeRowsToContents()
+
+    layout.addWidget(QLabel("Output:"))
+    layout.addWidget(table)
+
+    QApplication.processEvents()
+    window.update()
+
+
+
+file = "./data/test.txt"
+
+input_button : QPushButton
+file_label : QLabel
+
+def button_click():
+    global file
+    # while layout.count():
+    #     child = layout.takeAt(0)
+    #     if child.widget() and not isinstance(child.widget(), QPushButton):
+    #         child.widget().deleteLater()
+    file, _ = QFileDialog.getOpenFileName()
+    print(f"{file}")
+    file_label.setText(f'Selected file: "{file}"')
+
+def execute_button():
+    global file
+    if not file : return
+    while layout.count():
+        child = layout.takeAt(0)
+        if child.widget() and not isinstance(child.widget(), QPushButton):
+            child.widget().deleteLater()
+
+    layout.addStretch()
     rerank(args)
+
+if __name__ == '__main__':
+    input_btn = QPushButton("Open Input File")
+    input_btn.clicked.connect(button_click)
+
+    exec_btn = QPushButton("Execute File")
+    exec_btn.clicked.connect(execute_button)
+
+    file_label = QLabel(f"Selected file: {file}")
+
+
+    layout.addWidget(input_btn)
+    layout.addWidget(exec_btn)
+    layout.addWidget(file_label)
+    # layout.addStretch()
+
+    layout.addStretch()
+
+    # rerank(args)
+    sys.exit(app.exec())
+
